98a45e0ee0ceccc800160c5e9d84cd8936e02bde
ian <ian@ians-MacBook-Pro-2.local>
2023-04-22 06:34:28 -0700

    Update Home with components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from '../components/Header';
+import Main from '../components/Main';
+import Footer from '../components/Footer';
 
 export default function Home() {
     return (
         <div>
             <h1>Home</h1>
+            <Header />
+            <Main />
+            <Footer />
         </div>
     );
 }
