bcbd2e9d99965e1bd06b34cda6ea146fc9ecb3a9
ian <ian@ians-MacBook-Pro-2.local>
2023-04-21 19:36:46 -0700

    Add routes and pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Retail from './pages/Retail';
+import Membership from './pages/Membership';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Retail} />
+        <Route path="/retail" component={Retail} />
+        <Route path="/membership" component={Membership} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
