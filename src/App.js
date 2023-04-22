1b024d292d7b1d939683a5da403b9c347134537c
ian <ian@ians-MacBook-Pro-2.local>
2023-04-21 21:07:17 -0700

    Remove unused files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Retail from './pages/Retail';
-import Membership from './pages/Membership';
 
 function App() {
   return (
