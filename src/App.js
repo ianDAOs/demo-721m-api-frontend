a273584598abc61dd1938c0d99b61e668f3fb745
ian <ian@ians-MacBook-Pro-2.local>
2023-04-22 06:18:39 -0700

    Fixed About page path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import About from '.pages/About';
+import About from './pages/About';
 
 export default function App() {
   return (
