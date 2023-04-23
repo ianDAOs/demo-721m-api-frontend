8a31026b5097fedd189332ac553e11d891659e2d
ian <ian@ians-MacBook-Pro-2.local>
2023-04-23 08:49:12 -0700

    Change fetch to parent component and pass to child

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Title from './Title';
 import Nft from './Nft';
 import Metadata from './Metadata';
 import Button from './Button';
 
 export default function Main() {
+
+    const [metadata, setMetadata] = useState(null);
+
+    // Fetch the title from the Syndicate API and set it in state
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://m-demo-384522.uc.r.appspot.com');
+                const data = await response.json();
+                setMetadata(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <div>
-            <Title />
+            <Title metadata={metadata} />
             <Nft />
             <Metadata />
             <Button />
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,27 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
-export default function Title() {
+export default function Title({ metadata }) {
 
-    const [title, setTitle] = useState('');
+    // const [title, setTitle] = useState('');
 
-    // Fetch the title from the Syndicate API and set it in state
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('https://m-demo-384522.uc.r.appspot.com');
-                const data = await response.json();
-                setTitle(data.name);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-        fetchData();
-    }, []);
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await fetch('https://m-demo-384522.uc.r.appspot.com');
+    //             const data = await response.json();
+    //             setTitle(data.name);
+    //         } catch (error) {
+    //             console.error('Error fetching data:', error);
+    //         }
+    //     };
+    //     fetchData();
+    // }, []);
 
     return (
         <div>
-            <h1>{title}</h1>
-            <p>Gold L3 XP65</p>
+            <h1>{metadata.title}</h1>
+            <p>{metadata.badge} L{metadata.level} XP{metadata.XP}</p>
         </div>
     );
 }
