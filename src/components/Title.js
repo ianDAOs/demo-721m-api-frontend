ff913a9d41b21dd43dc2b50f45ee83f3623bdaa3
ian <ian@ians-MacBook-Pro-2.local>
2023-04-23 09:09:29 -0700

    Remove extra code from Title component

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,21 +2,6 @@ import React from 'react';
 
 export default function Title({ metadata }) {
 
-    // const [title, setTitle] = useState('');
-
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         try {
-    //             const response = await fetch('https://m-demo-384522.uc.r.appspot.com');
-    //             const data = await response.json();
-    //             setTitle(data.name);
-    //         } catch (error) {
-    //             console.error('Error fetching data:', error);
-    //         }
-    //     };
-    //     fetchData();
-    // }, []);
-
     return (
         <div>
             <h1>{metadata.name}</h1>
