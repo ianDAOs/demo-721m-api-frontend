35d545ef8aaec195ed47b9f8ce5c40714b96a92e
ian <ian@ians-MacBook-Pro-2.local>
2023-04-24 10:16:01 -0700

    Move steps array outside of functional component Loading

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import styles from './Loading.module.css';
 
+const steps = [
+    'Calling proxy server...',
+    'Calling Syndicate API...',
+    'Finding ERC-721M contract...',
+    'Finding token...',
+    'Updating token metadata...',
+    'Uploading image...',
+    'Mirroring image to IPFS...',
+    'Fetching metadata...'
+];
+
 export default function Loading() {
     const [currentStep, setCurrentStep] = useState(0);
     const [completedSteps, setCompletedSteps] = useState([]);
-    const steps = [
-        'Calling proxy server...',
-        'Calling Syndicate API...',
-        'Finding ERC-721M contract...',
-        'Finding token...',
-        'Updating token metadata...',
-        'Uploading image...',
-        'Mirroring image to IPFS...',
-        'Fetching metadata...'
-    ];
 
     useEffect(() => {
         if (currentStep < steps.length) {
