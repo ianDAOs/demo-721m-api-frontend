2c67baddb866eb03529f06dd5617316c04894e9b
ian <ian@ians-MacBook-Pro-2.local>
2023-04-24 12:34:33 -0700

    Add visualization to MainModify for testing

--- src/components/MainModify.js
+++ src/components/MainModify.js
@@ -12,6 +12,29 @@ export default function MainModify({ metadata: initialMetadata, buttonLabel }) {
         <div>
             <Title metadata={metadata} />
             <Nft />
+            <div>
+                <p>
+                    Token
+                    {'{'}
+                    chainId: 1,
+                    contractAddress: "0xca206cc53e533838f9edc4c25605ec475f0bbcef",
+                    tokenId: "1",
+                    {'}'}
+                </p>
+                <p>
+                    Metadata
+                    {'{'}
+                    "name":"{metadata.name}",
+                    "image":"TBD",
+                    "style":"{metadata.style}",
+                    "color":"{metadata.color}",
+                    "badge":"{metadata.badge}",
+                    "level":"{metadata.level}",
+                    "XP":"{metadata.XP}",
+                    "updated":"{metadata.updated}"
+                    {'}'}
+                </p>
+            </div>
             <Options metadata={metadata} setMetadata={setMetadata} />
             <Button metadata={metadata} buttonLabel={buttonLabel} />
         </div>
