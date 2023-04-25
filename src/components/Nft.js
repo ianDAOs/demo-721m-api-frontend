fffaf586486fc6b52695d37e3ed1adec240b7d0c
ian <ian.john.lee@gmail.com>
2023-04-24 20:14:44 -0700

    Fix link in Nft component

--- src/components/Nft.js
+++ src/components/Nft.js
@@ -4,7 +4,7 @@ export default function Nft() {
     return (
         <div>
             <p>/NFT image/</p>
-            <p>View on <a target="_blank" href='https://opensea.io/assets/ethereum/0xca206cc53e533838f9edc4c25605ec475f0bbcef/1'>Opensea</a> IPFS</p>
+            <p>View on <a href='https://opensea.io/assets/ethereum/0xca206cc53e533838f9edc4c25605ec475f0bbcef/1'>Opensea</a> IPFS</p>
             <p>Note: Updates may take time to appear on other platforms</p>
         </div>
     );
