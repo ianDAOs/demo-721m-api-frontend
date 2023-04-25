1f81fb888e551f3a0d2fc2b9d20cb7bb0cc83f16
ian <ian.john.lee@gmail.com>
2023-04-24 20:17:29 -0700

    Removed metadata from MainModify component

--- src/components/MainModify.js
+++ src/components/MainModify.js
@@ -12,29 +12,6 @@ export default function MainModify({ metadata: initialMetadata, buttonLabel }) {
         <div>
             <Title metadata={metadata} />
             <Nft />
-            <div>
-                <p>
-                    Token
-                    {'{'}
-                    chainId: 1,
-                    contractAddress: "0xca206cc53e533838f9edc4c25605ec475f0bbcef",
-                    tokenId: "1",
-                    {'}'}
-                </p>
-                <p>
-                    Metadata
-                    {'{'}
-                    "product":"{metadata.product}",
-                    "image":"TBD",
-                    "style":"{metadata.style}",
-                    "color":"{metadata.color}",
-                    "badge":"{metadata.badge}",
-                    "level":"{metadata.level}",
-                    "xp":"{metadata.xp}",
-                    "updated":"{metadata.updated}"
-                    {'}'}
-                </p>
-            </div>
             <Options metadata={metadata} setMetadata={setMetadata} />
             <Button metadata={metadata} buttonLabel={buttonLabel} />
         </div>
