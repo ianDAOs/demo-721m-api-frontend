36aca36da5fa07c1f84323b6cec0d909fc099edf
ian <ian.john.lee@gmail.com>
2023-04-25 19:51:09 -0700

    Fix fetch route

--- src/services/fetchMetadataService.js
+++ src/services/fetchMetadataService.js
@@ -3,7 +3,7 @@ import { BASE_URL } from '../data/config';
 
 export const fetchMetadata = async () => {
     try {
-        const response = await axios.get(`${BASE_URL}/update-metadata`);
+        const response = await axios.get(`${BASE_URL}/get-metadata`);
         return response.data;
     } catch (error) {
         console.error('Error fetching data:', error);
