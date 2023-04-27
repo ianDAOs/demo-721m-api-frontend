57b28275bd5e169654bc7963a0888618a26ee38c
ian <ian.john.lee@gmail.com>
2023-04-27 12:24:36 -0700

    Fix helper function

--- src/helpers/findAttribute.js
+++ src/helpers/findAttribute.js
@@ -1,4 +1,8 @@
 export const findAttribute = (attributes, traitType) => {
+    if (!attributes) {
+        return '';
+    }
+
     const attribute = attributes.find(attr => attr.trait_type === traitType);
     return attribute ? attribute.value : '';
 };
