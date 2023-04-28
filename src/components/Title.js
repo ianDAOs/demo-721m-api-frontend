d835388aad064b273ffc8b11ecfb91427b2d0e41
ian <ian.john.lee@gmail.com>
2023-04-28 16:08:14 -0700

    Fix Title styling

--- src/components/Title.js
+++ src/components/Title.js
@@ -7,7 +7,7 @@ export default function Title({ metadata }) {
         return (
             <div>
                 <h1 className="text-center text-4xl font-thin text-white pt-8 pb-2">Loading...</h1>
-                <p className="text-center text-md font-thin pb-12">Loading...</p>
+                <p className="text-center text-md font-thin text-white pb-12">Loading...</p>
             </div>
         );
     }
@@ -20,7 +20,7 @@ export default function Title({ metadata }) {
     return (
         <div>
             <h1 className="text-center text-4xl font-thin text-white pt-8 pb-2">{metadata.description}</h1>
-            <p className="text-center text-md font-thin pb-12">{badge} L{level} XP{xp}</p>
+            <p className="text-center text-md font-thin text-white pb-12">{badge} L{level} XP{xp}</p>
         </div>
     );
 }
