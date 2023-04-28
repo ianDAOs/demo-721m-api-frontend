fbe2fa72e0c6b38ce9c267d97c5b2dde3048ab4b
ian <ian.john.lee@gmail.com>
2023-04-28 14:52:55 -0700

    Add Tailwind CSS and style Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,91 @@
-import React from 'react';
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+    { name: 'Retail', href: '#' },
+    { name: 'Gaming', href: '#' },
+]
 
 export default function Header() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
     return (
-        <div>
-            <p>Retail</p>
-            <p>About</p>
-        </div>
-    );
+        <header className="bg-black">
+
+            <nav className="mx-auto flex items-center justify-between p-8 px-12" aria-label="Global">
+
+                <div className="flex items-center gap-x-10">
+
+                    <div className="hidden sm:flex gap-x-16">
+                        {navigation.map((item) => (
+                            <a key={item.name} href={item.href} className="text-sm font-extralight leading-6 text-gray-400 hover:text-white">
+                                {item.name}
+                            </a>
+                        ))}
+                    </div>
+
+                    <div className="flex sm:hidden">
+                        <button
+                            type="button"
+                            className="-m-3 inline-flex items-center justify-center rounded-md p-2.5 text-white"
+                            onClick={() => setMobileMenuOpen(true)}
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                    </div>
+
+                </div>
+
+                <div className="hidden sm:flex">
+                    <a href="#" className="text-sm font-extralight leading-6 text-gray-400 hover:text-white">
+                        Learn More
+                    </a>
+                </div>
+
+            </nav>
+
+            <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+                <div className="fixed inset-0 z-10" />
+                <Dialog.Panel className="fixed inset-y-3 right-0 z-10 w-full overflow-y-auto bg-black px-10 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="flex items-center justify-start">
+
+                        <button
+                            type="button"
+                            className="-m-2.5 rounded-md p-1.5 text-white"
+                            onClick={() => setMobileMenuOpen(false)}
+                        >
+                            <span className="sr-only">Close menu</span>
+                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                    </div>
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="space-y-2 py-6">
+                                {navigation.map((item) => (
+                                    <a
+                                        key={item.name}
+                                        href={item.href}
+                                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-extralight leading-7 text-gray-200 hover:bg-gray-50 hover:text-black"
+                                    >
+                                        {item.name}
+                                    </a>
+                                ))}
+                            </div>
+                            <div className="py-6">
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-extralight leading-7 text-gray-400 hover:bg-gray-50 hover:text-black"
+                                >
+                                    Learn More
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </Dialog.Panel>
+            </Dialog>
+
+        </header >
+    )
 }
