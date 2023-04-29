c5df16781467126cfdde3154d8058cfa7c04cc69
ian <ian.john.lee@gmail.com>
2023-04-29 08:11:23 -0700

    Add mx-auto to structure

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ export default function Title({ metadata }) {
 
     if (!metadata || !metadata.attributes) {
         return (
-            <div>
+            <div className="mx-auto">
                 <div className="pt-8"></div>
                 <button className="rounded-full bg-sky-500 text-center text-xs font-thin text-white w-30 p-2 px-4 mx-auto block">ERC-721M NFT</button>
                 <h1 className="text-center text-4xl font-thin text-white pt-8 pb-2">Loading...</h1>
@@ -20,8 +20,9 @@ export default function Title({ metadata }) {
     const xp = findAttribute(metadata.attributes, 'XP');
 
     return (
-        <div>
-            <p className="rounded-full bg-sky-500 text-center text-xs font-thin text-white p-2 px-4 mx-auto my-auto block">ERC-721M NFT</p>
+        <div className="mx-auto">
+            <div className="pt-8"></div>
+            <p className="rounded-full bg-sky-500 text-center text-xs font-thin text-white w-30 p-2 px-4 mx-auto my-auto block">ERC-721M NFT</p>
             <h1 className="text-center text-4xl font-thin text-white pt-8 pb-2">{metadata.description}</h1>
             <p className="text-center text-md font-thin text-white pb-12">{badge} L{level} XP{xp}</p>
         </div>
