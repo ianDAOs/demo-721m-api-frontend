f06a8bd4afbf0a96a0738b0f66da3680a8fe4771
ian <ian.john.lee@gmail.com>
2023-04-29 08:09:13 -0700

    Change 721m icon width

--- src/components/Title.js
+++ src/components/Title.js
@@ -7,7 +7,7 @@ export default function Title({ metadata }) {
         return (
             <div>
                 <div className="pt-8"></div>
-                <button className="rounded-full bg-sky-500 text-center text-xs font-thin text-white p-2 px-4 mx-auto block">ERC-721M NFT</button>
+                <button className="rounded-full bg-sky-500 text-center text-xs font-thin text-white w-30 p-2 px-4 mx-auto block">ERC-721M NFT</button>
                 <h1 className="text-center text-4xl font-thin text-white pt-8 pb-2">Loading...</h1>
                 <p className="text-center text-md font-thin text-white pb-12">Loading...</p>
             </div>
