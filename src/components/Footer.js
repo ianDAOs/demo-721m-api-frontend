933382c8eab93a08679e483d4378d16e3f913ec4
ian <ian.john.lee@gmail.com>
2023-05-02 16:41:50 -0700

    Remove Footer italic

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
     return (
         <div className='pt-12 pb-14 text-center font-thin text-white'>
             <p className='pb-4 text-3xl'>✺</p>
-            <p className='px-6 text-xs italic'>This demo is built on ERC-721M and Syndicate{`'`}s APIs {`(`}<a href={SYN_API_URL} className='text-sky-400'>view documentation</a>{`)`} with only React, Javascript, CSS, and HTML. <Link to='/about' className='text-sky-400'>Click here to Learn More.</Link></p>
+            <p className='px-6 text-xs'>This demo is built on ERC-721M and Syndicate{`'`}s APIs {`(`}<a href={SYN_API_URL} className='text-sky-400'>view documentation</a>{`)`} with only React, Javascript, CSS, and HTML. <Link to='/about' className='text-sky-400'>Click here to Learn More.</Link></p>
         </div>
     );
 }
