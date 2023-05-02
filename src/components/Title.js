b08053da42c4e1e79ef1108105065e259afcc6ee
ian <ian.john.lee@gmail.com>
2023-05-02 10:52:32 -0700

    Change label size

--- src/components/Title.js
+++ src/components/Title.js
@@ -7,7 +7,7 @@ export default function Title({ metadata }) {
         return (
             <div>
                 <div className='pt-8'></div>
-                <button className='mx-auto block rounded-full bg-sky-500 text-center text-xs font-thin text-white p-2 px-4'>ERC-721M NFT</button>
+                <button className='mx-auto block rounded-full bg-sky-500 text-center text-sm font-thin text-white p-2 px-4'>ERC-721M NFT</button>
                 <h1 className='text-center text-4xl font-thin text-white pt-8 pb-2'>Loading...</h1>
                 <p className='text-center text-md font-thin text-stone-400 pb-12'>Loading...</p>
             </div>
