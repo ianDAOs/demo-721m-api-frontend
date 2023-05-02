44919f996fcc5f3fd29cbad10fa5fd1aac138665
ian <ian.john.lee@gmail.com>
2023-05-02 10:43:49 -0700

    Edit Footer font size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <div className='overflow-auto pt-8 pb-14 text-center font-thin text-white'>
             <p className='pb-4 text-3xl'>✺</p>
-            <p className='text-sm italic'>This demo {`(`}<a href={FRONTEND_REPO_URL} className='text-sky-400'>frontend repo</a>{`)`} is built with ERC-721M and Syndicate{`'`}s APIs {`(`}<a href={SYN_API_URL} className='text-sky-400'>documentation</a>{`)`}</p>
+            <p className='px-6 text-xs italic'>This demo {`(`}<a href={FRONTEND_REPO_URL} className='text-sky-400'>frontend repo</a>{`)`} is built with ERC-721M and Syndicate{`'`}s APIs {`(`}<a href={SYN_API_URL} className='text-sky-400'>documentation</a>{`)`}</p>
         </div>
     );
 }
