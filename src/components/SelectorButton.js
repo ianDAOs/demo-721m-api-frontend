2b462155e1391bed46de9ed0ce94d24032bac603
ian <ian.john.lee@gmail.com>
2023-05-02 13:50:41 -0700

    Change Selector style

--- src/components/SelectorButton.js
+++ src/components/SelectorButton.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function SelectorButton({ buttonLabel, onClick }) {
     return (
-        <button onClick={onClick} className='text-sky-400 text-sm font-light'>{buttonLabel}</button>
+        <button onClick={onClick} className='text-emerald-400 text-sm font-light'>{buttonLabel}</button>
     );
 }
