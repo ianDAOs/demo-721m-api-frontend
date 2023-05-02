6a8620bf6b287f7b301b586f457c3ef41150c5ce
ian <ian.john.lee@gmail.com>
2023-05-02 15:14:01 -0700

    Update footnote

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { SYN_API_URL, FRONTEND_REPO_URL } from '../data/config';
 
 export default function Footer() {
     return (
         <div className='pt-12 pb-14 text-center font-thin text-white'>
             <p className='pb-4 text-3xl'>✺</p>
-            <p className='px-6 text-xs italic'>This demo {`(`}<a href={FRONTEND_REPO_URL} className='text-sky-400'>frontend repo</a>{`)`} is built with ERC-721M and Syndicate{`'`}s APIs {`(`}<a href={SYN_API_URL} className='text-sky-400'>documentation</a>{`)`}</p>
+            <p className='px-6 text-xs italic'>This demo is built with ERC-721M and Syndicate{`'`}s APIs {`(`}<a href={SYN_API_URL} className='text-sky-400'>view documentation</a>{`)`} with only React, Javascript, CSS, and HTML. <Link to='/about' className='text-sky-400'>Click here to Learn More.</Link></p>
         </div>
     );
 }
