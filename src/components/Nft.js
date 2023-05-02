31ba5989c0eebfe3ac899b471b5f61b1ef4b588a
ian <ian.john.lee@gmail.com>
2023-05-02 08:46:15 -0700

    Add Etherscan link

--- src/components/Nft.js
+++ src/components/Nft.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { contractAddress, tokenId } from '../data/contractData';
+import { ES_URL } from '../data/config';
 import loadingImage from '../assets/images/image_loading.png';
 
 export default function Nft({ metadata }) {
@@ -18,7 +19,7 @@ export default function Nft({ metadata }) {
                 ) : (
                     <img src={loadingImage} className='md:pl-6 sm:pl-0' alt='NFT loading visual' />
                 )}
-                <p className='text-center pt-6 text-sm font-extralight'>View on <a href={osUrl} className='text-sky-400'>Opensea</a> and <a href={metadata.image} className='text-sky-400'>IPFS</a></p>
+                <p className='text-center pt-6 text-sm font-extralight'>View on <a href={osUrl} className='text-sky-400'>Opensea</a> :: <a href={metadata.image} className='text-sky-400'>IPFS</a> :: <a href={ES_URL} className='text-sky-400'>Etherscan</a></p>
                 <p className='text-center pt-2 pb-8 text-xs font-extralight text-stone-400'>Note: Updates may take time to appear on other platforms</p>
             </div>
         </div>
--- src/data/config.js
+++ src/data/config.js
@@ -1,1 +1,3 @@
 export const BASE_URL = 'https://m-demo-384522.uc.r.appspot.com';
+
+export const ES_URL = 'https://etherscan.io/nft/0xca206cc53e533838f9edc4c25605ec475f0bbcef/1';
