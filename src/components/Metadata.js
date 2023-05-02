82588853bd51077efd7fda5f8b6e6c009a91b6fe
ian <ian.john.lee@gmail.com>
2023-05-02 08:18:13 -0700

    Style metadata object

--- src/components/Metadata.js
+++ src/components/Metadata.js
@@ -8,22 +8,22 @@ export default function Metadata({ metadata }) {
             <div>
                 <p>{'{'} </p>
                 <div className='pl-4'>
-                    <span className=' text-sky-400'>"chainId"</span>
-                    <span className='text-white'>: </span>
+                    <span className=' text-violet-200'>"chainId"</span>
+                    <span>: </span>
                     <span className='text-rose-400'>{chainId}</span>
-                    <span className='text-white'>,</span>
+                    <span>,</span>
                 </div>
                 <div className='pl-4'>
-                    <span className='text-sky-400'>"contractAddress"</span>
-                    <span className='text-white'>: </span>
+                    <span className='text-violet-200'>"contractAddress"</span>
+                    <span>: </span>
                     <span className='text-emerald-300'>"{contractAddress}"</span>
-                    <span className='text-white'>,</span>
+                    <span>,</span>
                 </div>
                 <div className='pl-4'>
-                    <span className='text-sky-400'>"tokenId"</span>
-                    <span className='text-white'>: </span>
+                    <span className='text-violet-200'>"tokenId"</span>
+                    <span>: </span>
                     <span className='text-emerald-300'>"{tokenId}"</span>
-                    <span className='text-white'>,</span>
+                    <span>,</span>
                 </div>
                 <p>{'}'} </p>
             </div>
@@ -32,47 +32,74 @@ export default function Metadata({ metadata }) {
                 <div className='pt-8'>
                     <p>{'{'} </p>
                     <div className='pl-4'>
-                        <span className='text-sky-400'>"description"</span>
-                        <span className='text-white'>: </span>
+                        <span className='text-violet-200'>"description"</span>
+                        <span>: </span>
                         <span className='text-emerald-300'>"{metadata.description}"</span>
-                        <span className='text-white'>,</span>
+                        <span>,</span>
                     </div>
                     <div className='pl-4'>
-                        <span className='text-sky-400'>"image"</span>
-                        <span className='text-white'>: </span>
+                        <span className='text-violet-200'>"image"</span>
+                        <span>: </span>
                         <span className='text-emerald-300'>"{metadata.image}"</span>
-                        <span className='text-white'>,</span>
+                        <span>,</span>
                     </div>
                     <div className='pl-4'>
-                        <span className='text-sky-400'>"updated"</span>
-                        <span className='text-white'>: </span>
+                        <span className='text-violet-200'>"updated"</span>
+                        <span>: </span>
                         <span className='text-emerald-300'>"{metadata.updated}"</span>
-                        <span className='text-white'>,</span>
+                        <span>,</span>
                     </div>
-                    <p className='pl-4'> "attributes": [
-                        {metadata.attributes && metadata.attributes.map((attr, index) => (
-                            <React.Fragment key={index}>
-                                <span>
-                                    {"{"}
-                                    "trait_type": "{attr.trait_type}",
-                                    "value": {JSON.stringify(attr.value)},
-                                    {attr.display_type && (
-                                        <>
-                                            "display_type": "{attr.display_type}",
-                                        </>
-                                    )}
-                                    {attr.max_value && (
-                                        <>
-                                            "max_value": {attr.max_value},
-                                        </>
-                                    )}
-                                    {"}"}
-                                </span>
-                                {index !== metadata.attributes.length - 1 && <>, </>}
-                            </React.Fragment>
-                        ))}
-                        ]
-                    </p>
+
+                    <div className='pl-4'>
+                        <span className='text-violet-200'>"attributes"</span>
+                        <span>: {`[`}</span>
+                    </div>
+                    {metadata.attributes && metadata.attributes.map((attr, index) => (
+                        <React.Fragment key={index}>
+                            <span className='pl-4'>
+                                {"{"}
+                                <div className='pl-4'>
+                                    <span className='text-violet-200'>"trait_type"</span>
+                                    <span>: </span>
+                                    <span className='text-emerald-300'>"{attr.trait_type}"</span>
+                                    <span>,</span>
+                                </div>
+                                <div className='pl-4'>
+                                    <span className='text-violet-200'>"value"</span>
+                                    <span>: </span>
+                                    <span className='text-emerald-300'>"{JSON.stringify(attr.value)}"</span>
+                                    <span>,</span>
+                                </div>
+                                {attr.display_type && (
+                                    <>
+                                        <div className='pl-4'>
+                                            <span className='text-violet-200'>"display_type"</span>
+                                            <span>: </span>
+                                            <span className='text-emerald-300'>"{attr.display_type}"</span>
+                                            <span>,</span>
+                                        </div>
+                                    </>
+                                )}
+                                {attr.max_value && (
+                                    <>
+                                        <div className='pl-4'>
+                                            <span className='text-violet-200'>"max_value"</span>
+                                            <span>: </span>
+                                            <span className='text-emerald-300'>"{attr.max_value}"</span>
+                                            <span>,</span>
+                                        </div>
+                                    </>
+                                )}
+                                {"}"}
+                            </span>
+                            {index !== metadata.attributes.length - 1 && <>, </>}
+                        </React.Fragment>
+                    ))}
+
+                    <div className='pl-4'>
+                        <span className='text-white'>{`]`}</span>
+                    </div>
+
                     <p>{'}'} </p>
                 </div>
 
