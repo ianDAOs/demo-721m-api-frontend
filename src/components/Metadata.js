317a4fb254461a168da9d5fe2ba2509e42baab64
ian <ian.john.lee@gmail.com>
2023-05-02 08:32:50 -0700

    Add conditional coloring

--- src/components/Metadata.js
+++ src/components/Metadata.js
@@ -66,7 +66,7 @@ export default function Metadata({ metadata }) {
                                 <div className='pl-4'>
                                     <span className='text-violet-200'>"value"</span>
                                     <span>: </span>
-                                    <span className='text-rose-300'>{JSON.stringify(attr.value)}</span>
+                                    <span className={typeof attr.value === 'number' ? 'text-rose-400' : 'text-emerald-300'}>{JSON.stringify(attr.value)}</span>
                                     <span>,</span>
                                 </div>
                                 {attr.display_type && (
