1db33265f33a310c9d7d086cf573976276468de1
ian <ian.john.lee@gmail.com>
2023-05-02 09:48:01 -0700

    Change label color

--- src/components/Nft.js
+++ src/components/Nft.js
@@ -15,7 +15,7 @@ export default function Nft({ metadata }) {
         <div className='flex md:justify-end sm:justify-center text-white'>
             <div>
                 <div className='xl:pl-0 md:pl-6 sm:pl-0 pl-6 pb-2'>
-                    <button className='rounded-full outline outline-1 outline-stone-400 px-4 py-2 text-xs font-thin text-white'>NFT Image</button>
+                    <button className='rounded-full outline outline-1 outline-stone-600 px-4 py-2 text-xs font-thin text-white'>NFT Image</button>
                 </div>
                 {metadata.image ? (
                     <img src={metadata.image} className='md:pl-6 sm:pl-0' alt='NFT visual' />
@@ -23,7 +23,7 @@ export default function Nft({ metadata }) {
                     <img src={loadingImage} className='md:pl-6 sm:pl-0' alt='NFT loading visual' />
                 )}
                 <p className='text-center pt-6 text-sm font-extralight'>View on <a href={osUrl} className='text-sky-400'>Opensea</a> :: <a href={metadata.image} className='text-sky-400'>IPFS</a> :: <a href={ES_URL} className='text-sky-400'>Etherscan</a></p>
-                <p className='text-center pt-2 pb-8 text-xs font-extralight text-stone-400'>Note: Updates may take time to appear on other platforms</p>
+                <p className='text-center pt-2 pb-12 text-xs font-extralight text-stone-400'>Note: Updates may take time to appear on other platforms</p>
             </div>
         </div>
     );
