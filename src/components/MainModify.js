69745b9147eeab58befb3fb970920605c99b2a69
ian <ian.john.lee@gmail.com>
2023-05-04 16:36:55 -0700

    Add transition to Modify component

--- src/components/MainModify.js
+++ src/components/MainModify.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import Title from './Title';
 import Nft from './Nft';
 import Options from './Options';
 import Button from './Button';
+import '../styles/component-fade.css';
 
 // Component for rendering the main section of the website with the NFT image, metadata modify options, and buttons
 export default function MainModify({ metadata: initialMetadata, buttonLabel, token }) {
@@ -18,8 +20,12 @@ export default function MainModify({ metadata: initialMetadata, buttonLabel, tok
                     <Nft metadata={metadata} token={token} />
                 </div>
                 <div className='md:pl-12 pl-6 md:pr-20 pr-6 whitespace-normal overflow-wrap break-all'>
-                    <Options metadata={metadata} setMetadata={setMetadata} token={token} />
-                    <Button metadata={metadata} token={token} buttonLabel={buttonLabel} />
+                    <CSSTransition in={true} classNames="component-fade" appear={true} timeout={2000}>
+                        <Options metadata={metadata} setMetadata={setMetadata} token={token} />
+                    </CSSTransition>
+                    <CSSTransition in={true} classNames="component-fade" appear={true} timeout={2000}>
+                        <Button metadata={metadata} token={token} buttonLabel={buttonLabel} />
+                    </CSSTransition>
                 </div>
             </div>
         </div>
