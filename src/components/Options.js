d9ffc2f3b7342c63563144ccf6ed43812637821b
ian <ian.john.lee@gmail.com>
2023-05-05 07:20:15 -0700

    Optimize Options

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,42 +3,55 @@ import Selector from './Selector';
 import { styles, colors, events, types } from '../data/optionsData';
 import { findAttribute } from '../helpers/findAttribute';
 
+// Helper function for getting the options for modifying the NFT's metadata
+const getOptions = (token) => {
+    if (token === 2) {
+        return {
+            firstOption: 'Event',
+            secondOption: 'Type',
+            firstOptionsArray: events,
+            secondOptionsArray: types,
+        };
+    } else {
+        return {
+            firstOption: 'Color',
+            secondOption: 'Style',
+            firstOptionsArray: colors,
+            secondOptionsArray: styles,
+        };
+    }
+};
+
 // Component for rendering the options for modifying the NFT's metadata
 export default function Options({ metadata, setMetadata, token }) {
-
-    let firstOption, secondOption, firstOptionsArray, secondOptionsArray, initialFirstOption, initialSecondOption;
-
-    // Check which token is being modified
-    if (token === 1) {
-        firstOption = 'Color';
-        secondOption = 'Style';
-        firstOptionsArray = colors;
-        secondOptionsArray = styles;
-    } else if (token === 2) {
-        firstOption = 'Event';
-        secondOption = 'Type';
-        firstOptionsArray = events;
-        secondOptionsArray = types;
-    } else {
-        firstOption = 'Color';
-        secondOption = 'Style';
-        firstOptionsArray = colors;
-        secondOptionsArray = styles;
-    }
+    const {
+        firstOption,
+        secondOption,
+        firstOptionsArray,
+        secondOptionsArray,
+    } = getOptions(token);
 
     // Get the initial values of the first and second attributes being changed
-    initialFirstOption = metadata ? findAttribute(metadata.attributes, firstOption) : '';
-    initialSecondOption = metadata ? findAttribute(metadata.attributes, secondOption) : '';
+    const initialFirstOption = metadata
+        ? findAttribute(metadata.attributes, firstOption)
+        : '';
+    const initialSecondOption = metadata
+        ? findAttribute(metadata.attributes, secondOption)
+        : '';
 
     // React hooks for setting the current values of the first and second attributes being changed
-    const [currentFirstOption, setCurrentFirstOption] = useState(initialFirstOption);
-    const [currentSecondOption, setCurrentSecondOption] = useState(initialSecondOption);
+    const [currentFirstOption, setCurrentFirstOption] = useState(
+        initialFirstOption
+    );
+    const [currentSecondOption, setCurrentSecondOption] = useState(
+        initialSecondOption
+    );
 
     // Update the metadata state with the current values of the first and second attributes while preserving other metadata fields and values
     useEffect(() => {
-        setMetadata(prevMetadata => ({
+        setMetadata((prevMetadata) => ({
             ...prevMetadata,
-            attributes: prevMetadata.attributes.map(attr => {
+            attributes: prevMetadata.attributes.map((attr) => {
                 if (attr.trait_type === firstOption) {
                     return { ...attr, value: currentFirstOption };
                 } else if (attr.trait_type === secondOption) {
@@ -47,7 +60,13 @@ export default function Options({ metadata, setMetadata, token }) {
                 return attr;
             }),
         }));
-    }, [firstOption, secondOption, currentFirstOption, currentSecondOption, setMetadata]);
+    }, [
+        firstOption,
+        secondOption,
+        currentFirstOption,
+        currentSecondOption,
+        setMetadata,
+    ]);
 
     return (
         <div>
